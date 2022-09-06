40b61b56ce34a303cf588e75b373cd8396feaa12
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2022-09-06 14:48:02 -0300

    Added face matching for user authentication

--- src/controllers/LoginUserController.ts
+++ src/controllers/LoginUserController.ts
@@ -3,10 +3,10 @@ import { LoginUserService } from "../services/LoginUserService"
 
 export class LoginUserController {
   async handle(request: Request, response: Response) {
-    const { login, password } = request.body
+    const { login, name, password } = request.body
 
     const service = new LoginUserService()
-    const result = await service.execute(login, password)
+    const result = await service.execute(login, name, password)
 
     return response.status(result.code).json(result.data)
   }
--- src/services/LoginUserService.ts
+++ src/services/LoginUserService.ts
@@ -3,7 +3,7 @@ import { compareHash } from "../utils/bcryption"
 import { signToken } from "../utils/signJSONtoken"
 
 export class LoginUserService {
-  async execute(login: string, password: string) {
+  async execute(login: string, name: string, password: string) {
     // If patter exists, it means login is an email
     // This way user can login with either their email or nickname, for instance
     const patternExists = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
@@ -15,7 +15,11 @@ export class LoginUserService {
         where: patternExists ? { email: login } : { nickname: login }
       })
 
-      if (user && (await compareHash(password, user.password))) {
+      if (
+        user &&
+        user.name === name &&
+        (await compareHash(password, user.password))
+      ) {
         const token = signToken(user)
         return { data: { token, user }, code: 200 }
       }
